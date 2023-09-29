7cd78f4c86b41270fd7ad9e6ec7e4a711a13c523
dnguy <deniis69@outlook.fr>
2023-09-29 15:00:26 +0200

    Initial commit

---
+++ src/MontantValidator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Nguyen\TestMontantFrais;
+
+class MontantValidator
+{
+    public static function validerMontant($montant)
+    {
+        return is_numeric($montant) && $montant >= 0.01 && $montant <= 200;
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nguyen/test-montant-frais',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '91da20adad559c14a668219869d91f53e244a64a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nguyen/test-montant-frais' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '91da20adad559c14a668219869d91f53e244a64a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
